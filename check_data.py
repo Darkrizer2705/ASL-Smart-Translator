5a6ced7baa6c7bac2836b02bb820925a38b83edd
Darkrizer2705 <Shrivatshks@gmail.com>
2026-05-13 19:51:05 +0530

    change

--- check_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('datasets/gesture_dataset.csv')
-print(f'Total rows: {len(df)}')
-print(f'Phrases: {df["label"].nunique()}')
-print(f'\nSamples per phrase:')
-print(df['label'].value_counts().to_string())
